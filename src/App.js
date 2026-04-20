11a274350f6057fa0d9ebb900fc64b3de7d3c13e
Nate Alleyne <ncgalleyne@gmail.com>
2026-04-20 15:16:32 -0400

    dummy app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Mobile App</h1>
+        <p>This is a dummy landing page to test the mobile app banner.</p>
+
+        <button className="cta-button">
+          Download App
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
